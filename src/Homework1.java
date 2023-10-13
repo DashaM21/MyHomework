b19ab246dc1178b2d8c7edbc767683dd46b6e0b8
dasha <mmenshykova20.10@gmail.com>
2023-10-14 02:02:19 +0300

    first homework

---
+++ src/Homework1.java
@@ -0,0 +1,10 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        a =b;
+        b = a-2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
