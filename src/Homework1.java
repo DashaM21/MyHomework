94ba0284f695b0117848fbf29720bc1aac24eb46
dasha <mmenshykova20.10@gmail.com>
2023-10-14 20:59:19 +0300

    fix first homework

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,9 +2,9 @@ public class Homework1 {
     public static void main(String[] args) {
         int a = 7;
         int b = 9;
-        a =b;
-        b = a-2;
+        a = b;
         System.out.println(a);
+        b = --a - a/--a;
         System.out.println(b);
     }
 }
