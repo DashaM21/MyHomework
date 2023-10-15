6da1e08974ced9baec4df2017b2d96d1a2efae64
dasha <mmenshykova20.10@gmail.com>
2023-10-15 21:34:47 +0300

    homework2

---
+++ src/Homework1/Homework1.java
@@ -0,0 +1,13 @@
+package Homework1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+            int a = 7;
+            int b = 9;
+            a = b;
+            System.out.println(a);
+            b = --a - a/--a;
+            System.out.println(b);
+    }
+}
+
